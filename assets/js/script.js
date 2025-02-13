87e47969fa0a19c973b4a24bfa0d463c8bf45b32
ZeeshanMughaal <115975972+ZeeshanMughaal@users.noreply.github.com>
2025-02-13 16:08:28 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -569,7 +569,10 @@ function showProjects(projects) {
   JSON.parse(projects).filter((project) => project.category === "android").forEach((project) => {
     var img="";
     if(project.image=='TwoFactorAuthenticationPackageForSuiteCRM8'){
-       img=`<img draggable="false" src="assets/images/projects/${project.image}.png" alt="project" />`;
+       img=`<video controls draggable="false">
+                            <source src="assets/images/projects/${project.image}.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>`;
     }else{
       img=`<img draggable="false" src="assets/images/projects/${project.image}.png" alt="project" />`;
     }
